1d0b38627f45bf09e00d96f9e3fb3013a7243096
akhramova <xaa2128@mail.ru>
2024-02-27 16:34:37 +0300

    Add design

---
+++ src/main/java/ru/akhramova/tacocloud/model/Taco.java
@@ -0,0 +1,11 @@
+package ru.akhramova.tacocloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
